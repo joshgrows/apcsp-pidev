3c880ae896b2942ba1a0a92ae10996332ceed758
Joshua Grozen <jgrozen@att.net>
2019-05-24 16:22:58 +0000

    finished the comparison example code

--- cdemo/ifstmnt.c
+++ cdemo/ifstmnt.c
@@ -3,7 +3,7 @@
 int main()
 {
   int a = 0;
-
+  int b = 2;
   // if statement to test is a is equal to 0
   if (a == 0)
   {
@@ -13,5 +13,52 @@ int main()
   {
     printf("a is not 0\n");
   }
-    
+if (a != b)
+{
+printf("a is not equal to b\n");
+}
+else
+{
+printf("a is equal to b\n");
+}
+if (a > b)
+{
+printf("a is greater than b\n");
+}
+else
+{
+printf("a is not greater than b\n");
+}
+if (a >= b)
+{
+printf("a is greater than or equal to b\n");
+}
+else
+{
+printf("a is not greater than or equal to b\n");
+}
+if (a  == 0 && b == 0)
+{
+printf("a and b are equal to 0\n");
+}
+else
+{
+printf("a and b are not equal to 0\n");
+}
+if (a == 0 || b == 0)
+{
+printf("a b or both are equal to 0\n");
+}
+else
+{
+printf("neither a or b are equal to 0\n");
+}
+if (!(a == 0))
+{
+printf("a is not equal to 0\n");
+}
+else
+{
+printf("a is equal to 0\n");
+}
 }
--- cdemo/sizeofvariables.c
+++ cdemo/sizeofvariables.c
@@ -3,8 +3,17 @@
 int main()
 {
   int a = 545;
-
+float b = 12.34;
+char c = 4;
+double d = 123.456;
+unsigned int e = 4;
+signed int f = 4;
   // print value and size of an int variable
   printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+printf("float b value: %d and size: %d bytes\n", b, sizeof(b));
+printf("chr c value: %d and size: %d bytes\n", c, sizeof(c));
+printf("double d value: %d and size: %d bytes\n", d, sizeof(d));
+printf("int e value: %d and size: %d bytes\n", e, sizeof(e));
+printf("int f value: %d and size: %d bytes\n", f, sizeof(f));
 }
 
